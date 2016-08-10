f4aa816342439c805f846c09631c6d6aee128e15
Jeremy Robles <jeremy.robles56@gmail.com>
2016-08-10 14:23:39 -0700

    updated connection

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 var express = require('express');
-var routes = require('./routes');
 var cylon = require('cylon');
+var path = require("path");
+var http = require("http");
+var curl = require("curlrequest");
 var app = express();
 
 cylon.api({
-	host: '127.0.0.1',
-	post: '4040',
-	ssl: false,
-	auth:false
+    host: '127.0.0.1',
+    post: '4040',
+    ssl: false,
+    auth: false
 });
 
 var robot = cylon.robot({
-	name: 'omnius',
-	connections: {
-		arduino: {adaptor: 'firmata', port: '/dev/cu.usbmodem1421'}
-	},
-	work: function (){
-	
-	},
-	commands: function(){
-		return {
-			on: this.On,
-			off: this.Off
-		}
-	}, 
-	On: function(){
-		robot.connections.arduino.pinMode(13, 'OUTPUT');
-		robot.connections.arduino.digitalWrite(13, 1);
-		console.log('turning the light on');
-	},
-	Off: function(){
-		robot.connections.arduino.pinMode(13, 'OUTPUT');
-		robot.connections.arduino.digitalWrite(13, 0);
-		console.log('turning the light off');
-	}
+    name: 'omnius',
+    connections: {
+        arduino: {adaptor: 'firmata', port: '/dev/cu.usbmodem1421'}
+    },
+    work: function () {
+
+    },
+    commands: function () {
+        return {
+            on: this.On,
+            off: this.Off
+        }
+    },
+    On: function () {
+        robot.connections.arduino.pinMode(13, 'OUTPUT');
+        robot.connections.arduino.digitalWrite(13, 1);
+        console.log('turning the light on');
+    },
+    Off: function () {
+        robot.connections.arduino.pinMode(13, 'OUTPUT');
+        robot.connections.arduino.digitalWrite(13, 0);
+        console.log('turning the light off');
+    }
 });
 robot.start();
 
-
-
-app.use('/', routes);
+app.get('/api/robots/omnius/commands/on', function (req, res) {
+    res.sendStatus(200);
+    curl.request('http://localhost:4040/api/robots/omnius/commands/on', function (err) {
+        // console.log(err);
+    });
+});
+app.get('/api/robots/omnius/commands/off', function (req, res) {
+    res.sendStatus(200);
+    curl.request('http://localhost:4040/api/robots/omnius/commands/off', function (err) {
+        // console.log(err);
+    });
+});
 
 app.listen(3000);
 console.log('Serving over port 3000');
--- routes.js
+++ routes.js
@@ -9,15 +9,15 @@ module.exports = (function() {
 
     var router = require('express').Router();
 
-    router.get('/on', function(req, res){
+    router.get('/api/robots/omnius/commands/on', function(req, res){
 	res.sendStatus(200);
-	curl.request('http://localhost:4040/api/robots/omnius/commands/on', function(err){
+	curl.request('http://localhost:4041/api/robots/omnius/commands/on', function(err){
 		console.log(err);
 	});
     });
-    router.get('/test', function(req, res){
+    router.get('/api/robots/omnius/commands/off', function(req, res){
 	res.sendStatus(200);
-	curl.request('http://localhost:4040/api/robots/omnius/commands/off', function(err){
+	curl.request('http://localhost:4041/api/robots/omnius/commands/off', function(err){
 		console.log(err);
 	});
     });
