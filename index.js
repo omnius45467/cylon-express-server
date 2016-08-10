8db597a2d4167df57641857382766ecde5e6b36a
Jeremy Robles <jeremy.robles56@gmail.com>
2016-08-10 10:10:12 -0700

    init commit

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 var express = require('express');
 var routes = require('./routes');
-
+var cylon = require('cylon');
 var app = express();
+
+cylon.api({
+	host: '127.0.0.1',
+	post: '4040',
+	ssl: false,
+	auth:false
+});
+
+var robot = cylon.robot({
+	name: 'omnius',
+	connections: {
+		arduino: {adaptor: 'firmata', port: '/dev/cu.usbmodem1421'}
+	},
+	work: function (){
+	
+	},
+	commands: function(){
+		return {
+			on: this.On,
+			off: this.Off
+		}
+	}, 
+	On: function(){
+		robot.connections.arduino.pinMode(13, 'OUTPUT');
+		robot.connections.arduino.digitalWrite(13, 1);
+		console.log('turning the light on');
+	},
+	Off: function(){
+		robot.connections.arduino.pinMode(13, 'OUTPUT');
+		robot.connections.arduino.digitalWrite(13, 0);
+		console.log('turning the light off');
+	}
+});
+robot.start();
+
+
 
 app.use('/', routes);
 
--- routes.js
+++ routes.js
@@ -2,16 +2,24 @@
  * Created by jeremyrobles on 8/8/16.
  */
 var path = require("path");
+var http = require("http");
+var curl = require("curlrequest");
 
 module.exports = (function() {
 
     var router = require('express').Router();
 
-    router.get('/', function(req, res){
-        res.sendFile(path.join(__dirname+'/markup/index.html'));
+    router.get('/on', function(req, res){
+	res.sendStatus(200);
+	curl.request('http://localhost:4040/api/robots/omnius/commands/on', function(err){
+		console.log(err);
+	});
     });
     router.get('/test', function(req, res){
-        res.sendFile(path.join(__dirname+'/markup/index.html'));
+	res.sendStatus(200);
+	curl.request('http://localhost:4040/api/robots/omnius/commands/off', function(err){
+		console.log(err);
+	});
     });
     
     return router;
